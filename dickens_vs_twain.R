450b9cde3be02b4aa61bf7a5a52fa7cc92a26012
burgikukac <tamas.burghard@gmail.com>
2018-02-23 14:24:21 +0100

    First version to submit as a homework of the Dickens vs Twain

--- dickens_vs_twain.R
+++ dickens_vs_twain.R
@@ -1,3 +1,18 @@
+## Homework 2 
+#
+#  DS of Unstructured Text Data held by Eduardo Ariño de la Rubia on CEU in 2018
+#  Tamas Burghard
+#
+#  Comparing the Volumes of letters of Mark Twain and Charles Dickens
+#  With word frequencies and TF-IDF
+#  
+#  I thought that these volumes of letters are more interesting than just some romances
+#
+#  Input: raw text files from Project Gutenberg (6 + 3 files)
+#   
+#  
+
+
 library(gutenbergr)
 library(tidyverse)
 library(tidytext)
@@ -6,33 +21,38 @@ library(stopwords)
 library(reshape2)
 library(RColorBrewer)
 
-
+## creating the list of volumes to download
 
 twain_list <- gutenberg_works() %>% 
   filter(str_detect(author, "Twain, Mark"),
          str_detect(title, "Letters"),
-         !str_detect(title, "Complete"))
+         !str_detect(title, "Complete"))  #  this would contain the previous ones
   
 dickens_list <- gutenberg_works() %>% 
   filter(str_detect( author, "Dickens, Charles"), 
          str_detect(title, "Letters"))
 
+## downloading the volumes, creating line- and letternumbers with regexp
 
 twain_raw <- gutenberg_download(as.vector(twain_list$gutenberg_id)) %>% 
+  mutate(gutenberg_id = paste0("Twain ",gutenberg_id)) %>%  # more informative
   group_by(gutenberg_id) %>% 
   mutate(author = "Twain",
-         linenumber = row_number(),
-         letternumber = cumsum(str_detect(text, regex("^To.*:$")))) %>% 
-  filter(letternumber > 0) %>% 
+         linenumber = row_number(),  #creating rownumbers in volumes
+         letternumber = cumsum(str_detect(text, regex("^To.*:$")))) %>%  # identifying the first row of a letter
+  filter(letternumber > 0) %>%  # before the first letter is not important for us (like preface)
   ungroup()
 
 dickens_raw <- gutenberg_download(as.vector(dickens_list$gutenberg_id)) %>% 
+  mutate(gutenberg_id = paste0("Dickens ",gutenberg_id)) %>% 
   group_by(gutenberg_id) %>% 
   mutate(author = "Dickens",
          linenumber = row_number(),
-         letternumber = cumsum(str_detect(text, regex("^\\[Sidenote")))) %>% 
+         letternumber = cumsum(str_detect(text, regex("^\\[Sidenote")))) %>% # identifying the first row of a letter
   filter(letternumber > 0) %>% 
   ungroup()
+
+## creating a tidy text tibble, keeping just the alphabetical information (and: '), removing stop words
 
 together_tidy <- rbind(twain_raw, dickens_raw) %>% 
   unnest_tokens(word, text) %>% 
@@ -40,15 +60,14 @@ together_tidy <- rbind(twain_raw, dickens_raw) %>%
   filter(!is.na(word)) %>% 
   anti_join(stop_words)
 
-word_freq <- together_tidy %>% 
+## creating word frequencies / proportions by author and by volume (book)
+
+word_freq_by_author <- together_tidy %>% 
   count(author, word) %>%
   group_by(author) %>%
   mutate(proportion = n / sum(n)) %>% 
   select(-n) %>% 
-  ungroup()
-
-
-word_freq_by_author <- word_freq %>% 
+  ungroup() %>% 
   spread(author, proportion)
 
 word_freq_by_book <- together_tidy %>% 
@@ -59,14 +78,12 @@ word_freq_by_book <- together_tidy %>%
   ungroup() %>% 
   spread(gutenberg_id, proportion)
 
+## how much correlate the two authors and the volumes?
 
-#head(word_freq)
+print("Correlation between ALL the letters of Dickens / Twain")
+cor.test(word_freq_by_author$Dickens, word_freq_by_author$Twain)
 
-cor.test(word_freq_by_author$Dickens, word_freq_by_author$Twain         )
-
-data <-cor(word_freq_by_book[,-1], use = "pairwise")
-?cor.test
-
+# cor(word_freq_by_book[,-1], use = "pairwise")  # uncomment this line to see the corr. mtx
 
 data <- word_freq_by_book[,-1]
 qplot(x = as.factor(Var1), 
@@ -76,16 +93,51 @@ qplot(x = as.factor(Var1),
       main = "Correlation Plot between Dickens/Twain letter volumes",
       xlab = "Gutenberg id", 
       ylab = "Gutenberg id") +
-  scale_fill_distiller(palette = "BrBG", limits = c(-1,1))
-  
-  
-#25852 is strange - too much intro? subscript?
+  scale_fill_distiller(palette = "BrBG", limits = c(0,1))  # limit is intentionally from 0
 
-book_words <- together_tidy %>% 
+#  correlation plots usually don't give much information, in our case the stronger correlation
+#  between two volumes of the same author is clear. 3193 and 25853 seem to be different
+#  3193: most probably because of the introduction
+
+
+## TF-IDF, by books
+
+(book_words <- together_tidy %>% 
   rename(book = gutenberg_id) %>% 
   count(book, word, sort = TRUE) %>% 
   ungroup() %>% 
   bind_tf_idf(word, book, n) %>% 
-  arrange(desc(tf_idf))
+  arrange(desc(tf_idf)) %>% 
+  top_n(15)
+)
+
+## the most powerful words: vo (volume), sidenote (addressing that Dickens used)
+## clemens (twain's original name), dickens, twain
+## TODO : remove these using custom stopwords
+
+to_remove <- c("vo", "sidenote", "vols", "ii", "dickens", "twain", "clemens")
+
+## TF-IDF, by books  WITH new stopwords
+
+(book_words2 <- together_tidy %>% 
+    filter(!word %in% to_remove) %>%  # filtering only the new stopwords
+    rename(book = gutenberg_id) %>% 
+    count(book, word, sort = TRUE) %>% 
+    ungroup() %>% 
+    bind_tf_idf(word, book, n) %>% 
+    group_by(book) %>% 
+    arrange(desc(tf_idf)) %>% 
+    top_n(5) %>% 
+    ungroup()
+)
+
+book_words2  %>%
+  ggplot(aes(word, tf_idf, fill =tf_idf)) + 
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~book, scales = "free_y") + 
+  labs(y = "TF-IDF", x = NULL) + 
+  coord_flip() + 
+  ggtitle("Top 5 words with the highest TF-IDF by volumes \n (after removing custom stopwords)")
+
 
 
