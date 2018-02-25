f315fc4cf161c0cd24b3860a0f7f87bcff12c765
burgikukac <tamas.burghard@gmail.com>
2018-02-25 22:44:26 +0100

    Topic modeling with more books

--- topic_modeling.R
+++ topic_modeling.R
@@ -8,10 +8,9 @@
 #  Ideally the topics algorithm identify these 3 categories - or gives
 #  some other insight
 #
-#  Input: raw text files from Project Gutenberg (6 + 3 files)
+#  Input: raw text files from Project Gutenberg
 #   
 #  
-
 
 library(gutenbergr)
 library(tidyverse)
@@ -20,29 +19,25 @@ library(stringr)
 library(stopwords)
 library(topicmodels)
 
-library(reshape2)
-library(RColorBrewer)
-
-
 rm(list=ls())
 
 ## Creating lists of Gutenberg_id-s by categories
 
-history_list <- gutenberg_subjects %>% 
+subjects <- gutenberg_subjects %>%   # this prefilter is needed to avoid french books
+  inner_join(gutenberg_metadata) %>% 
+  filter(language == "en", has_text == T) 
+
+history_list <- subjects %>% 
   filter(str_detect(subject, "History")) %>%
-  filter(str_detect(subject, "English")) %>%
-  top_n(n = 10, -gutenberg_id)
+  top_n(n = 30, gutenberg_id)
 
-politics_list <- gutenberg_subjects %>%
+politics_list <- subjects %>%
   filter(str_detect(subject, "Politics")) %>%
-  filter(gutenberg_id != 20046) %>%
-  top_n(n = 10, gutenberg_id)
+  top_n(n = 30, gutenberg_id)
 
-love_list <- gutenberg_subjects %>% 
+love_list <- subjects %>% 
   filter(str_detect(subject, "Romance")) %>%
-  filter(str_detect(subject, "English")) %>%
-#  filter(gutenberg_id != 31100) %>%  # was too long
-  top_n(n = 10, -gutenberg_id)
+  top_n(n = 30, gutenberg_id)
 
 
 
@@ -50,8 +45,7 @@ love_list <- gutenberg_subjects %>%
 ## Downloading the raw books, adding the categories to the id.
 
 history_raw <- gutenberg_download(as.vector(history_list$gutenberg_id)) %>% 
-  mutate(gutenberg_id = paste0("History ",gutenberg_id))  
-  # more informative
+  mutate(gutenberg_id = paste0("History ",gutenberg_id))  # more informative
 
 politics_raw <- gutenberg_download(as.vector(politics_list$gutenberg_id)) %>% 
   mutate(gutenberg_id = paste0("Politics ",gutenberg_id))  # more informative
@@ -64,7 +58,7 @@ love_raw <- gutenberg_download(as.vector(love_list$gutenberg_id)) %>%
 ## Combining the 3 raw sets, and tidying
 
 raw <- rbind(history_raw, politics_raw, love_raw)
-##raw %>% group_by(gutenberg_id) %>% summarise(N = n()) %>% ungroup() %>% ggplot(aes(factor(gutenberg_id), N)) + geom_bar() + coord_flip()
+
 to_remove <- c("thou", "de", "thy", "thee")  # custom stopwords
 
 tidy <- raw %>% unnest_tokens(word, text)  %>% 
@@ -73,6 +67,8 @@ tidy <- raw %>% unnest_tokens(word, text)  %>%
   filter(!is.na(word)) %>% 
   anti_join(stop_words)
 
+
+## basic word frequencies plot
 
 word_freq_by_book <- tidy %>% 
   count(gutenberg_id, word) %>%
@@ -92,32 +88,39 @@ wc <- tidy %>%
 
 LDA_result <- LDA(wc, k = 3, control = list (seed = 20180225))
 
-LDA_result
-
 topics <- tidy(LDA_result, matrix = "beta")
 
 top_terms <- topics %>% 
   group_by(topic) %>% 
-  top_n(10, beta) %>% 
+  top_n(8, beta) %>% 
   ungroup() %>% 
   arrange(topic, -beta)
 
 top_terms
+
+## graphs 
 
 top_terms %>% 
   mutate(term = reorder(term, beta)) %>% 
   ggplot(aes(term, beta, fill = factor(topic))) +
   geom_col(show.legend = FALSE) +
   facet_wrap(~ topic, scales = "free") +
-  coord_flip()
+  coord_flip() +
+  ggtitle("The strongest words of the topics") 
+
+## I was thinking about removing 'ii' and 'iii' with stopwords, but these words can represent difference between
+## historical text and romance
 
 tidy(LDA_result, matrix = "gamma") %>% 
   separate(document, c("Type", "ID"), sep = " ", convert = TRUE) %>% 
   mutate(Type = reorder(Type, gamma * topic)) %>% 
   ggplot(aes(factor(topic), gamma)) + 
   geom_boxplot() +
-  facet_wrap(~ Type)
+  facet_wrap(~ Type) + 
+  labs(title = "Original subjects VS LDA Topics", 
+       x = "Topic",
+       caption = "What can I say?  \n History is a mix of love and politics...")
 
-
+## What can I say? History is a mix of love and politics...
 
 
