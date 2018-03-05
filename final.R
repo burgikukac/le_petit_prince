0665dd2093dda0f2f15f2ad7c1fe834e3bd8693e
burgikukac <tamas.burghard@gmail.com>
2018-03-05 18:39:44 +0100

    extracting publish dates

--- final.R
+++ final.R
@@ -9,14 +9,19 @@ library(RColorBrewer)
 library(urltools)
 library(rvest)
 
+rm(list = ls())
+
+
 authors <- gutenberg_authors %>% 
   filter(deathdate > 1850)
 
 meta  <- gutenberg_metadata %>% 
   filter(language == "en",
-         has_text == TRUE) %>% 
-  group_by(has_text) %>% 
-  summarise(N = n())
+         has_text == TRUE)
+
+#%>% 
+#  group_by(has_text) %>% 
+#  summarise(N = n())
 
 meta            
 
@@ -61,7 +66,7 @@ look_this <- meta3 %>% sample_n(10)
 url1 <- "https://www.worldcat.org/search?qt=worldcat_org_bks&q="
 
 url2 <- paste0("ti:", url_encode( look_this$title))
-url3 <- paste0("+au:", ulr_encode(look_this$author))
+url3 <- paste0("+au:", url_encode(look_this$author))
 
 url4 <- "&qt=advanced&dblist=638"
 
@@ -71,8 +76,16 @@ raw2   <- tibble(v2, links = url) %>%
   mutate( webpages = map(links, read_html)) %>% 
   mutate( nodes = map(webpages, html_nodes, '.itemPublisher')) %>% 
   mutate( text = map(nodes, html_text)) 
+
+
 #%>% 
 #  select(languages, text)
+
+
+raw2   <- tibble(links = url) %>% 
+  mutate( webpages = map(links, read_html)) %>% 
+  mutate( nodes = map(webpages, html_nodes, '.itemPublisher')) %>% 
+  mutate( text = map(nodes, html_text)) 
 
 
 # url <- "http://en.wikipedia.org/wiki/api.php?action=parse&pageid=1023&export=json"
@@ -87,13 +100,24 @@ raw2   <- tibble(v2, links = url) %>%
 # url
 # # [1] "http://en.wikipedia.org/wiki/api.php?pageid=12"
 
-
-
-
+View(raw2$text[[9]])
+write.csv(raw2$links, "raw2.txt")
+str(raw2, max.level = 1)
 
 ## to extract year number
 ## input: messy string from scrape
 ## output: 4 digit number, starting with 1-2
 ## method: extracting from the last 6 characters
-s1 <- str_trunc(raw2$text[[2]], 6, side = "left", ellipsis = "")
-str_extract_all(s1, "[0-9]{4,4}")
+
+s1 <- str_trunc(raw2$text[[10]], 6, side = "left", ellipsis = "")
+min(as.numeric(str_extract_all(s1, "[0-9]{4,4}")), na.rm = TRUE)
+
+extract_publish <- function(RAW_TEXT_I) {
+  s1 <- str_trunc(RAW_TEXT_I, 6, side = "left", ellipsis = "")
+  min(as.numeric(str_extract_all(s1, "[0-9]{4,4}")), na.rm = TRUE)
+  
+}
+
+extract_publish(raw2$text)
+
+map(raw2$text, extract_publish)
