ce608dbc54e6b0eb358600cb076eafce0852f8d1
burgikukac <tamas.burghard@gmail.com>
2018-03-06 14:13:21 +0100

    first version of final.R that contains real analysis

--- final.R
+++ final.R
@@ -121,3 +121,108 @@ extract_publish <- function(RAW_TEXT_I) {
 #extract_publish(raw2$text)
 
 map(raw2$text, extract_publish)
+
+
+
+## we have 'looked' that contains the possible booklist with the metadata
+## now we download the books
+
+download_this <- looked %>% 
+  filter(year <1990)
+
+#final_raw_1990 <- gutenberg_download(as.vector(download_this$gutenberg_id))
+
+## we have final_raw_1990
+
+final_tidy_1990 <- final_raw_1990 %>% 
+  unnest_tokens(word, text)
+save(final_tidy_1990, file ="final_tidy_1990.RData")
+
+
+sentiments <- get_sentiments("afinn")
+
+after_sentiment <- final_tidy_1990 %>%
+  inner_join(meta3)
+
+wc_by_book <- final_tidy_1990  %>% 
+  group_by(gutenberg_id) %>% 
+  summarise(N_ALL = n()) %>% 
+  ungroup()
+
+senti_by_book <- final_tidy_1990 %>% 
+  inner_join(sentiments) %>% 
+  group_by(gutenberg_id) %>% 
+  summarise(sentiment = sum(score, na.rm = TRUE)) %>% 
+  left_join(wc_by_book) %>% 
+  mutate(avg_senti = round(sentiment / N_ALL, 3))
+
+
+senti_by_book_with_meta <- senti_by_book %>% 
+  left_join(looked)
+
+#--- senti with bing
+sentiments_nrc <- get_sentiments("nrc")
+
+senti_by_nrc_with_meta <- final_tidy_1990 %>% 
+  inner_join(sentiments_nrc) %>%
+  count(gutenberg_id, sentiment) %>% 
+  left_join(wc_by_book) %>% 
+  mutate(n = round(n / N_ALL, 3), N_ALL = NULL) %>%  
+  #spread(sentiment, n, fill = 0) %>% 
+  left_join(looked)
+
+  
+  
+  group_by(gutenberg_id) %>% 
+  summarise(sentiment = sum(score, na.rm = TRUE)) %>% 
+  left_join(wc_by_book) %>% 
+  mutate(avg_senti = round(sentiment / N_ALL, 3))
+
+
+senti_by_book_with_meta <- senti_by_book %>% 
+  left_join(looked)
+
+
+## original: meta3
+ggplot(senti_by_book_with_meta, aes(year, avg_senti)) +
+  geom_point() + 
+  geom_vline(xintercept = 1914, col="red") + 
+  geom_vline(xintercept = 1939, col="red") +
+  geom_vline(xintercept = 1861, col="red") +
+  geom_vline(xintercept = 1989, col="red") +
+  geom_vline(xintercept = 1962, col="red") +
+  
+  
+  geom_rect(xmin = 1914, xmax = 1918, 
+            ymin = -1, ymax = 1,
+            fill = "red", alpha = 0.003) + 
+  geom_rect(xmin = 1939, xmax = 1945, 
+            ymin = -1, ymax = 1,
+            fill = "red", alpha = 0.003) + 
+  geom_rect(xmin = 1861, xmax = 1865, 
+            ymin = -1, ymax = 1,
+            fill = "red", alpha = 0.003)
+
+
+
+
+
+ggplot(senti_by_nrc_with_meta, aes(year, n)) +
+  geom_point() + 
+  facet_wrap(~ sentiment) + 
+  geom_vline(xintercept = 1914, col="red") + 
+  geom_vline(xintercept = 1939, col="red") +
+  geom_vline(xintercept = 1861, col="red") +
+  geom_vline(xintercept = 1989, col="red") +
+  geom_vline(xintercept = 1962, col="red") +
+  
+  
+  geom_rect(xmin = 1914, xmax = 1918, 
+            ymin = -1, ymax = 1,
+            fill = "red", alpha = 0.003) + 
+  geom_rect(xmin = 1939, xmax = 1945, 
+            ymin = -1, ymax = 1,
+            fill = "red", alpha = 0.003) + 
+  geom_rect(xmin = 1861, xmax = 1865, 
+            ymin = -1, ymax = 1,
+            fill = "red", alpha = 0.003)
